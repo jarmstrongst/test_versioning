ee842f1948be3944892228892923c1af6c417b3f
jarmstrongst <jarmstrong@sharethrough.com>
2022-08-15 10:15:09 -0400

    first program ready for public release

--- program.py
+++ program.py
@@ -1,2 +1,15 @@
+class MathMachine():
+	
+	def __init__(self, x, y, z):
+		self.x = x
+		self.y = y
+		self.z = z
+	
+	def do_math_magic(self):
+		return self.x * self.y + self.z 
+
+
 if __name__ == '__main__':
 	print('hello world!')
+	machine_1 = MathMachine(1,2,3)
+	print(machine_1.do_math_magic())
